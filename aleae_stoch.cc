c78b5bc79122a3a28bc426b6e83a1f3e9f676775
thaisartell <thaisartell@gmail.com>
2026-02-23 10:16:30 -0600

    comment

--- aleae_stoch.cc
+++ aleae_stoch.cc
@@ -33,8 +33,8 @@ aleae_stoch(const biocr_t           biocr, // reactions
 
    // Event-count limit (for Problem 1b)
    // If time_lt <= -2, interpret -time_lt as max number of events
+   /*
    int event_lt = -1;
-   /* 
    if (param.time_lt <= -2.0)
     event_lt = (int)(-param.time_lt);
    */
